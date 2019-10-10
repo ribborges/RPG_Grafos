0049550db31248862cc38ed456b9473ad8ed97d1
Richard de Carvalho Borges <46366493+ribborges@users.noreply.github.com>
2019-10-10 15:28:37 -0300

    Mudanças

--- grafo.h
+++ grafo.h
@@ -1,2 +1,45 @@
 #include <stdio.h>
+#include <stdlib.h>
 
+int MainMenu ();
+int teste ();
+int ExitGraph ();
+
+int MainMenu ()
+{
+    int x;
+    system("cls");
+    printf("   ______________________________________________________________________________\n");
+    printf("   |                                                                            |\n");
+    printf("   |                       TRABALHO DE TEORIA DOS GRAFOS!                       |\n");
+    printf("   |          Alunos: Richard de Carvalho Borges, Thiago Fonsato Alves          |\n");
+    printf("   |                                                                            |\n");
+    printf("   |                                                                            |\n");
+    printf("   |  Escolha uma opcao?                                                        |\n");
+    printf("   |  1- Jogar                                                                  |\n");
+    printf("   |  2- Sair                                                                   |\n");
+    printf("   |____________________________________________________________________________|\n");
+    printf("    Resposta: ");
+    scanf("%d", &x);
+    printf("\n    Aguarde");
+    sleep(1);
+    printf(".");
+    sleep(1);
+    printf(".");
+    sleep(1);
+    printf(".");
+    sleep(1);
+    return x;
+}
+
+int teste ()
+{
+    system("cls");
+    system("COLOR 1F");
+    printf ("OK");
+}
+
+int ExitGraph ()
+{
+    exit(0);
+}
--- main.c
+++ main.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "grafo.h"
 
 main ()
 {
-    printf("Ola mundo");
-    sleep();
+    int x;
+    int graph[50][50];
+    x = MainMenu();
+
+    x == 1 ? teste() : ExitGraph();
+
+    sleep(50000);
 }
